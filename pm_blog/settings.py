0b64a9f5ce7d4b7455f17fd1e43f7114221e3e08
mixvert <bajkowski1e@gmail.com>
2023-11-28 23:23:38 +0100

    Fix error "django.core.exceptions.ImproperlyConfigured: WSGI application 'pm_blog.wsgi.application' could not be loaded; Error importing module.:"

--- pm_blog/settings.py
+++ pm_blog/settings.py
@@ -79,7 +79,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'pm_blog.urls'
